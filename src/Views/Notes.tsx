d2bd4606836f675e79be0963c017c85158074715
Alireza <Alirezahamame@gmail.com>
2024-07-21 11:44:05 -0800

    tooltip added

--- src/Views/Notes.tsx
+++ src/Views/Notes.tsx
@@ -1,22 +1,37 @@
-
 import NoteCardBox from "@/Components/NoteCard";
 import { Button } from "@/Components/ui/button";
-
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/Components/ui/tooltip";
 import { FaPen } from "react-icons/fa6";
 
 function Notes({ onClick }: { onClick: () => void }) {
   const tmepNotes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  
+
   return (
     <div className="grid grid-cols-5 gap-3 w-full p-3 relative">
       {tmepNotes.map((t) => (
         <NoteCardBox key={t} />
       ))}
 
-        <Button onClick={onClick} className="w-14 h-14 rounded-full fixed right-20 bottom-3">
-          <FaPen className="text-lg"/>
-        </Button>
-        
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              onClick={onClick}
+              className="w-14 h-14 rounded-full fixed right-20 bottom-3"
+            >
+              <FaPen className="text-lg" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add a note</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 }
