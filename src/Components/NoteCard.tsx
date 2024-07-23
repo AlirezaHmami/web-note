67edba1efec5694903b942854ee21a7573952e9f
Alireza <Alirezahamame@gmail.com>
2024-07-22 22:34:46 -0800

    noteCard remove and more added

--- src/Components/NoteCard.tsx
+++ src/Components/NoteCard.tsx
@@ -7,22 +7,24 @@ import {
   CardFooter,
   CardTitle,
 } from "./ui/card";
+import { MdDelete } from "react-icons/md";
+import { CgMoreO } from "react-icons/cg";
 
-
-function NoteCardBox({id , content , noteColor , title} : notesState) {
+function NoteCardBox({ id, content, noteColor, title }: notesState) {
   return (
-    <Card className={` bg-${noteColor}-100`}>
+    <Card className={` bg-${noteColor}-100 `}>
       <CardHeader>
-      <CardTitle className="">
-          Title : <CardDescription className="inline-block">{title}</CardDescription>
+        <CardTitle className="">
+          Title :
+          <CardDescription className="inline-block">{title}</CardDescription>
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <p className="line-clamp-6">{content}</p>
+        <p className="line-clamp-6 h-[144px]">{content}</p>
       </CardContent>
-      <CardFooter>
-        more
-        delet
+      <CardFooter className="flex items-center justify-between">
+        <CgMoreO className="text-2xl cursor-pointer text-black text-opacity-30 hover:text-opacity-70" />
+        <MdDelete className="text-2xl cursor-pointer text-black text-opacity-30 hover:text-opacity-70"  />
       </CardFooter>
     </Card>
   );
